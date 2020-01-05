3cc6d06a9e92708f133cd97ec3bc39b6de819096
Felipe <felipe1180@hotmail.com>
2020-01-05 20:30:27 -0300

    logs

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const app = express();
 
 mongoose.connect(
-  "mongodb+srv://fezinho11:123felao@cluster0-mtv2i.mongodb.net/test?retryWrites=true&w=majority"
+  "mongodb+srv://fezinho11:123felao@cluster0-mtv2i.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log(`MongoDB is working!`)
 );
 
 module.exports = app;
